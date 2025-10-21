66a8743b68c8346868790332303970e3a7810a1d
Julio Rodriguez Escogido <85967297+JulioRodriguez17@users.noreply.github.com>
2025-10-20 18:29:27 -0600

    Termino de clase 2 - FindElement

--- src/test/java/NavigationTest.java
+++ src/test/java/NavigationTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NavigationTest {
@@ -14,6 +16,18 @@ public class NavigationTest {
         driverUpdated.quit();
     }
 
+    @Test
+    public void fillOutRegistrationForm() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("file:///Users/julrodr117/Java/mini-web-apps/registration/register.html");
+        Thread.sleep(3000);
+
+        WebElement fieldname = driver.findElement(By.cssSelector("#name"));
+        fieldname.sendKeys("Test");
+        Thread.sleep(3000);
+
+        driver.quit();
+    }
 
 }
 
