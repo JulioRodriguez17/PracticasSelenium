fb882d6e84c1d37ad44bd2289689aaea91d1f58c
JulioRodriguez17 <agentebirds.333@gmail.com>
2025-10-21 11:23:02 -0600

    Termino clases teoricas del curso - Errores y etiquetas

--- src/test/java/NavigationTest.java
+++ src/test/java/NavigationTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -6,43 +8,69 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 import java.io.File;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class NavigationTest {
 
+    private static int counter1 = 0;
+    private static int counter2 = 0;
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setup(){
+        counter1++;
+        System.out.println("Ejecutando antes de cada test: "+counter1);
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void tearDown(){
+        counter2++;
+        System.out.println("Ejecutando despues de cada test: "+counter2);
+
+        if(driver != null){
+            driver.quit();
+        }
+    }
+
+
     @Test
     public void navigationToGoogle() throws InterruptedException {
-        WebDriver driverUpdated = new ChromeDriver();
-
-        driverUpdated.get("https://google.com");
-
+        driver.get("https://google.com");
         Thread.sleep(3000);
-        driverUpdated.quit();
     }
 
     @Test
     public void fillOutRegistrationForm() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
         //driver.get("file:///Users/julrodr117/Java/mini-web-apps/registration/register.html");
         driver.get("file:\\D:\\Badak\\Curso Selenium\\mini-web-apps\\registration\\register.html");
         Thread.sleep(3000);
 
+        String name = "Julio";
+        String lastName = "Rodr";
+        String age = "24";
+        String country = "Mexico";
+        String email = "johndue@gmail.com";
+
         WebElement fieldname = driver.findElement(By.cssSelector("#name"));
-        fieldname.sendKeys("Julio");
+        fieldname.sendKeys(name);
 
         WebElement fieldLastName = driver.findElement(By.cssSelector("#last-name"));
-        fieldLastName.sendKeys("Rodr");
+        fieldLastName.sendKeys(lastName);
 
         WebElement fieldAge = driver.findElement(By.cssSelector("#age"));
-        fieldAge.sendKeys("24");
+        fieldAge.sendKeys(age);
 
         Select selectCountry = new Select(driver.findElement(By.cssSelector("#country")));
-        selectCountry.selectByVisibleText("Mexico");
+        selectCountry.selectByVisibleText(country);
 
         WebElement fieldSex = driver.findElement(By.cssSelector("#sex-m"));
         fieldSex.click();
 
         WebElement fieldEmail = driver.findElement(By.cssSelector("#email"));
-        fieldEmail.sendKeys("johndue@gmail.com");
+        fieldEmail.sendKeys(email);
 
         WebElement checkMonday = driver.findElement(By.cssSelector("#monday"));
         checkMonday.click();
@@ -54,11 +82,54 @@ public class NavigationTest {
         driver.findElement(By.cssSelector("#picture")).sendKeys(profileImage.getAbsolutePath());
 
         WebElement saveButton = driver.findElement(By.cssSelector("#save-btn"));
+        Thread.sleep(3000);
         saveButton.click();
 
         Thread.sleep(3000);
-        driver.quit();
+
+        Object[] windowHandles = driver.getWindowHandles().toArray();
+        driver.switchTo().window((String) windowHandles[1]);
+
+        String currentName = driver
+                .findElement(By.cssSelector("#summary-details > p:nth-child(1)")).getText();
+
+        String currentlastName = driver
+                .findElement(By.cssSelector("#summary-details > p:nth-child(2)")).getText();
+
+        String currentAge = driver
+                .findElement(By.cssSelector("#summary-details > p:nth-child(3)")).getText();
+
+        String currentCountry = driver
+                .findElement(By.cssSelector("#summary-details > p:nth-child(4)")).getText();
+
+        String currentEmail = driver
+                .findElement(By.cssSelector("#summary-details > p:nth-child(6)")).getText();
+
+        assertThat(currentEmail).contains(email);
+        assertThat(currentCountry).contains(country);
+
+        Thread.sleep(3000);
+
     }
+
+    @Test
+    public void assertJTest() {
+        List<String> tools = List.of("Selenium", "Playwright", "RestAssured");
+    }
+
+    @Test
+    public void closeBrowserTest(){
+        //driver.get("file:///Users/julrodr117/Java/mini-web-apps/registration/register.html");
+        driver.get("file:\\D:\\Badak\\Curso Selenium\\mini-web-apps\\registration\\register.html");
+
+        String name = "Julio";
+
+        WebElement fieldname = driver.findElement(By.cssSelector("#foo"));
+        fieldname.sendKeys(name);
+    }
+
+
+
 
 }
 
